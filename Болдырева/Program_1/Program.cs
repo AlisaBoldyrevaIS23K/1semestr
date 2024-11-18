d06973f0be9443b98d594a55302231120a5fb686
AlisaBoldyrevaIS23K <alisaboldyreva7@gmail.com>
2024-11-18 08:47:29 +0300

    ПЗ-3

---
+++ Болдырева/Program_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 01_01.cs - первая программа
+using static System.Console;
+class HelloUser
+
+{
+   static void Main()
+   {
+       string name;
+       WriteLine("Введите ваше имяя: ");
+       name = ReadLine();
+       WriteLine("Приветствую Васс, " + name + "!");
+        WriteLine(" /infl Завершение сеанса нажмите ENTER. ");
+        ReadLine();
+   }
+}
