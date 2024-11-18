e033a98e9ff27791c3b931d73db074edf00d1d5a
AlisaBoldyrevaIS23K <alisaboldyreva7@gmail.com>
2024-11-18 09:12:10 +0300

    Add files via upload

---
+++ ПЗ12/PZ10БолдыреваШевченко/з1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace PZ10БолдыреваШевченко
+    {
+    class з1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Численность группы = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Возраст {0}-го человеака: ", i + 1);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double m = 0;
+            for (int i = 0; i < n; i++)
+                m = m + a[i];
+            m = m / n;
+                int min, max;
+            min = a[0];
+            max = a[0];
+            for (int i = 1; i < n; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+                if (a[i] > max)
+                    max = a[i];
+            }
+            Console.Write("ИТОГИ:\nВсего опрошено {0}\n", n);
+            Console.WriteLine("средний возраст = {0}", m);
+            Console.WriteLine("Минимальный возраст = {-1}", min);
+            Console.WriteLine("Максимальный возраст = {110}", max);
+            Console.ReadKey();
+        }
+    } 
+   }
---
+++ ПЗ12/задание1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PZ10БолдыреваШевченко
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Численность группы = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] ages = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                while (true) 
+                {
+                    Console.Write("Возраст {0}-го человека: ", i + 1);
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        ages[i] = Convert.ToInt32(input);                       
+                        if (ages[i] < 0 || ages[i] > 120)
+                        {
+                            Console.WriteLine("Пожалуйста, введите возраст от 0 до 120.");
+                            continue; 
+                        }
+                        break; 
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+                    }
+                }
+            }
+            double averageAge = 0;
+            for (int i = 0; i < n; i++)
+                averageAge += ages[i];
+
+            averageAge /= n;
+
+            int min = ages[0];
+            int max = ages[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (ages[i] < min)
+                    min = ages[i];
+                if (ages[i] > max)
+                    max = ages[i];
+            }
+
+            Console.WriteLine("ИТОГИ:");
+            Console.WriteLine("Всего опрошено: {0}", n);
+            Console.WriteLine("Средний возраст: {0:F2}", averageAge);
+            Console.WriteLine("Минимальный возраст: {0}", min);
+            Console.WriteLine("Максимальный возраст: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
