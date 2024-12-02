51c1d4a119c7cc82425f164a53e438b80f6c0882
AlisaBoldyrevaIS23K <alisaboldyreva7@gmail.com>
2024-12-02 11:09:08 +0300

    Add files via upload

---
+++ Болырева_ПЗ-20/Болырева_ПЗ-20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Болырева_ПЗ_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+             int sum()
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 0)
+                    return 0;
+
+                return number + sum();
+            }
+            {
+                Console.WriteLine("введите последовательность чисел, заканчивающуюся 0:");
+                int result = sum();
+                Console.WriteLine($"Сумма: {result}");
+            }
+        }
+    }
+}
